6b47e57849718d8afb56be08e25dbac2f415fe9c
Mark Holmes <mark.j.holmes.95@gmail.com>
2024-10-02 17:25:37 +0200

    now the species TPC parameters are generated according to the Rezende data. this is going to be unsafe at the moment because the ylims are not well defined i.e. 1

---
+++ src/generate-T-data.jl
@@ -0,0 +1,14 @@
+using CSV, Distributions, JLD2, DataFrames # todo stop this being a csv maybe? this is gross
+
+mean_dat = CSV.read("data/rezende2019-plant-means.csv", DataFrame)
+means = mean_dat.x
+covs = Matrix(CSV.read("data/rezende2019-plant-covs.csv", DataFrame)[1:4,2:5])
+
+save("data/T_dist.jld2",
+    Dict(
+        "T_dist" => MvNormal(means, covs),
+        "names" => Tuple(Symbol.(mean_dat.Column1))
+        )
+    )
+
+# todo include r script from playground that shows where this all came from
